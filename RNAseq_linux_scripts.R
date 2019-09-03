783349c9454f01408c3dd66936ea8578fb22dee6
zhuozhp <54835993+zhuozhp@users.noreply.github.com>
2019-09-03 16:50:02 +0800

    Update RNAseq_linux_scripts.R

--- RNAseq_linux_scripts.R
+++ RNAseq_linux_scripts.R
@@ -22,12 +22,6 @@ i=${i%R1_paired.fq*};
 nohup hisat2 -p 4--dta -x /public/resource/bdgp6_tran/genome_tran -1 ${i}R1_paired.fq -2 ${i}R2_paired.fq -S  /public/work/results/${i}align.sam 2>/public/work/fastqc/${i}align.log  & 
 done
 
-# samtools, in results/ dir
-for i in *.sam;
-do 
-i=${i%.align.sam*}; nohup samtools  view  -bS ${i}.align.sam > ${i}_unsorted.bam & 
-done
-
 # sam to sotrted.bam (in results/ dir) >1.3 version
 for i in *.sam;
 do 
@@ -59,6 +53,6 @@ do
 i=${i%.bam*}; nohup stringtie -p 8 -G /public/resource/gene/Drosophila_melanogaster.BDGP6.22.44.gtf -o /public/work/results/gtf/${i}.gtf -A /public/work/results/tab/${i}.tab -B -e -l ${i} ${i}.bam &
 done
 
-# under gtf/, build dir DESeq2/
+# under gtf/, build DESeq2/ dir
 # prepare gtflist.txt which contain sample gtf information and its path.
 python /public/resource/prepDE.py -i gtflist.txt -g DESeq2/gene_count.csv -t DESeq2/transcript.csv
